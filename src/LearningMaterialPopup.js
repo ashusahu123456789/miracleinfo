bfdd36819a734b59a7597eaad205a80ed9cd1d0d
ashutoshsahuvlogs <ashutoshsahuvlogs@gmail.com>
2025-06-10 15:23:08 +0530

    fixbug

--- src/LearningMaterialPopup.js
+++ src/LearningMaterialPopup.js
@@ -216,6 +216,24 @@ const LearningMaterialPopup = ({ onClose }) => {
             selectedOptions={selectedPreferences}
             setSelectedOptions={setSelectedPreferences}
           />
+          <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <button
+              type="button"
+              style={{
+                backgroundColor: '#007bff',
+                color: '#fff',
+                border: 'none',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                fontWeight: '600',
+                fontSize: '16px',
+              }}
+              onClick={() => alert('Next button clicked')}
+            >
+              Next
+            </button>
+          </div>
         </form>
       </div>
     </div>
